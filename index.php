f1192cc529f727b87a7eb8204861256c5dc4c22d
mak1220 <mkhan.1220@gmail.com>
2022-01-06 12:18:21 +0500

    just to test the upload scenario

--- index.php
+++ index.php
@@ -1,89 +1,55 @@
 <?php
-   require "mylib.php"
-?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title>	</title>
-</head>
-<body>
-   
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
 
-  <h1>
-   <?php
+define('LARAVEL_START', microtime(true));
 
-    echo "Welcome to first web page using PHP code";
+/*
+|--------------------------------------------------------------------------
+| Check If The Application Is Under Maintenance
+|--------------------------------------------------------------------------
+|
+| If the application is in maintenance / demo mode via the "down" command
+| we will load this file so that any pre-rendered content can be shown
+| instead of starting the framework, which could cause an exception.
+|
+*/
 
-   ?>
-   </h1>
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
 
-   <?php
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here so we don't need to manually load our classes.
+|
+*/
 
-    /*
-    $x = 40;
-    $y = 67;
+require __DIR__.'/../vendor/autoload.php';
 
-    $result = $x + $y;
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request using
+| the application's HTTP kernel. Then, we will send the response back
+| to this client's browser, allowing them to enjoy our application.
+|
+*/
 
-    echo "The addition of ".$x." and ".$y." is = ".$result."<br>";
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
-    echo "$x,$y,$result"."<br>";
-    echo $x,$y,$result;
-    echo "<br>";
-    echo "The value of x is ". $x;
-    print "The value of x is ". $x;
+$kernel = $app->make(Kernel::class);
 
-    $cars = array("Toyota","Mercedes","Nissan","City","Honda","BMW");
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
 
-    echo "<br>I like the ",$cars[3]." and ".$cars[1];
-
-    echo "<br>Total cars in the list is ",count($cars);
-
-    $length = count($cars);
-
-    for ($index=0; $index<$length; $index++)
-    {
-    	echo $cars[$index];
-    	echo "<br>";
-    }
-    */
-    $carspeed = array("bMW"=>"380","0Mercedes"=>"450","mehran"=>"180","Nissan"=>"480","Civic"=>"280","Honda"=>"185");
-
-    echo $carspeed["Mercedes"]."<br>";
-
-    ksort($carspeed);
-
-    
-    
-    foreach($carspeed as $carname=>$speed)
-    {
-      
-        echo "the speed of ".$carname." is ".$speed."<br>";
-      
-    }
-
-    
-    echo "<br>The sum of the 45 and 78 is ".sum(45,78);
-    echo "<br>The Product of the 45 and 20 is ".Multiply(45,20);
-    echo "<br>The Remainder of the 45 and 20 is ".Remainder(45,20);
-
-    echo "<br>".date("d/m/yy");
-    echo "<br>".date("d/m/yy",strtotime("-1 days"));
-    echo "<br>".date("l,F m,Y",strtotime("-5 days"));
-
-    
- //   echo "<br>the Multiplication of 23 and 56 is ". Multiply(23,56);
-
-
-
-    
-   ?>
-   <br>
-   This is the Advanced web technology class 
-   
-
-
-
-</body>
-</html>
+$kernel->terminate($request, $response);
